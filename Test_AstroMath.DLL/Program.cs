7029fc8d6d15b220fb879df9b972bac91badf865
JUNN30057205 <115967698+JUNN30057205@users.noreply.github.com>
2023-09-04 17:44:48 +0800

    AstroMath.DLL_Test_file

---
+++ Test_AstroMath.DLL/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AstroMath;
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
+namespace Test_AstroMath.DLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instance of class
+            CalculateAstroMath calculateAstroMath = new CalculateAstroMath();
+            Console.WriteLine("DLL test program");
+
+            //Star Velocity
+            Console.WriteLine("1. Calculate Star Velocity.");
+            double v = CalculateAstroMath.StarVelocity(500.1, 500.0);
+            Console.WriteLine( v + " m/s");      
+            Console.WriteLine();
+
+            //Star Distance
+            Console.WriteLine("2. Calculate Star Distance.");
+            double distanceParsec = CalculateAstroMath.StarDistance(0.547);
+            Console.WriteLine(distanceParsec + " parsec");
+            Console.WriteLine();
+
+            //Temparature in Kelvin
+            Console.WriteLine("3. Calculate Temparature in kelvin");
+            double kelvin = CalculateAstroMath.TemparatureCoveter(27.0);
+            Console.WriteLine(kelvin + " degrees");
+            Console.WriteLine();
+
+            //Event Horizon
+            Console.WriteLine("4. Calculate Event Horizon.");
+            double r = CalculateAstroMath.EventHorizon(8.2e36);
+            Console.WriteLine(r + " metres");
+            Console.WriteLine();
+
+        }
+    }
+}
