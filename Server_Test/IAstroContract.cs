87823418e1766a4f17d2ce4c5d260e47e0cb1248
JUNN30057205 <115967698+JUNN30057205@users.noreply.github.com>
2023-09-06 15:35:54 +0800

    create server test file

---
+++ Server_Test/IAstroContract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Server_Test
+{
+    [ServiceContract]
+    internal interface IAstroContract
+    {
+        [OperationContract]
+        //string ReverseString(string value);
+        double StarVelocity(double ObservedWavelength, double RestWavelength);
+
+
+
+    }
+}
---
+++ Server_Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = "net.pipe://localhost/";
+            NetNamedPipeBinding binding =
+            new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            EndpointAddress ep = new EndpointAddress(address);
+            IAstroContract channel = ChannelFactory<IAstroContract>.CreateChannel(binding, ep);
+
+            Console.WriteLine("1. Calculate Star Velocity.");
+            double v = channel.StarVelocity(500.1, 500.0);
+            Console.WriteLine(v + " m/s");
+            Console.WriteLine();
+        }
+    }
+}
